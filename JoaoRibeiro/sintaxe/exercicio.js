69a307b19f6ad8815703c4fbecdf1e4df56d1877
David Dutra <dadutra@hotmail.com>
2024-05-20 16:25:37 -0300

    Joao lesson 151 - Loops, for IN OF each

--- JoaoRibeiro/sintaxe/exercicio.js
+++ JoaoRibeiro/sintaxe/exercicio.js
@@ -5,7 +5,6 @@ console.log("Instrução 2");
 /*
 Inserindo comentários - multiline
 */
-
 var nome = "Joao Ribeiro";
 var idade = 50;
 var admin = true;
@@ -24,13 +23,35 @@ var c = 4e+2;
 
 
 // VAR tem escopo de função
-
 //LET e CONST tem escopo de bloco - não suportadas por navegadores antigos
-
-
 //Começar variáveis com letras _ ou $
 //Nunca começar com números!!
 
 //alert(nome)
 
-document.write(c)
+
+let nomes = ["joao", 'ana', 'josefina'];
+
+nomes.push('carlos', 'jose')
+
+//SHIFT remove do início - contraintuitivo
+nomes.unshift('INICIO')
+
+//remove ao fim e pega o elemento
+nomes.pop();
+
+console.table(nomes.sort())
+
+
+// //For IN - itera retornando cada índice (valor)
+// for(valor in nomes){
+//     console.log(valor)
+// }
+
+// //For OF itera no elemento (objeto em valor)
+// for(valor of nomes){
+//     console.log(valor)
+// }
+
+//Arrow function - ECMAS6
+nomes.forEach((n) => console.log(n))
