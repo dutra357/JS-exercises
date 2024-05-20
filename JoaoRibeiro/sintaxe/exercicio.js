2c55f33e3d5aedf604e5947b1115f713ca6d7584
David Dutra <dadutra@hotmail.com>
2024-05-20 09:57:51 -0300

    Joao lesson 137 - variables

--- JoaoRibeiro/sintaxe/exercicio.js
+++ JoaoRibeiro/sintaxe/exercicio.js
@@ -2,9 +2,29 @@ console.log("Instrução 1");
 console.log("Instrução 2");
 
 //inline
-
 /*
 Inserindo comentários - multiline
 */
 
+var nome = "Joao Ribeiro";
+var idade = 50;
+var admin = true;
 
+var cidade;
+cidade = "Porto";
+
+console.log(nome);
+console.log(cidade);
+
+//Após o ecma6 passou a ser possível declarar LET and CONST
+let texto = "Frase em LET";
+const pi = 3.14;
+
+
+// VAR tem escopo de função
+
+//LET e CONST tem escopo de bloco - não suportadas por navegadores antigos
+
+
+//Começar variáveis com letras _ ou $
+//Nunca começar com números!!
