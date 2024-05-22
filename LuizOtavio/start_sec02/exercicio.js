681a98a6c7a67b8220c675bd45edbe5c9b03164d
David Dutra <dadutra@hotmail.com>
2024-05-22 11:52:32 -0300

    Luiz Otavio lesson 44 - imc calc

--- LuizOtavio/start_sec02/exercicio.js
+++ LuizOtavio/start_sec02/exercicio.js
@@ -1,1 +1,9 @@
-console.log();
+let valor1 = document.querySelector("#input01").valor1;
+let valor2 = document.querySelector("#input02").valor2;
+
+let box2 = document.querySelector(".box2");
+
+function calcular(valor1, valor2){
+    return valor1 + valor2;
+}
+
