4bab5dc8fef72c1c29b3e9f8e6b2164d80c484bb
David Dutra <dadutra@hotmail.com>
2024-05-23 08:22:44 -0300

    Luiz Otavio lesson 063 - exception handler

--- LuizOtavio/start_sec02/exercicio.js
+++ LuizOtavio/start_sec02/exercicio.js
@@ -5,12 +5,30 @@ form.addEventListener('submit', function(e){
 
     let primeiro = e.target.querySelector('#input01').value;
     let segundo = e.target.querySelector('#input02').value;
-    
+
     //resultado(primeiro)
-    multiplica(primeiro, segundo)
+    try {
+        multiplica(primeiro, segundo)
+    } catch(e) {
+        console.log('Lançando erro!')
+    }
 })
 
+function multiplica(a, b) {
+    //input devolve STRING
+    if(typeof a == 'number') {
+        throw('Os valores precisam ser numéricos')
+    }
 
+    console.log(typeof a)
+
+    const resultado = document.querySelector('#box2');
+    const p = document.createElement('p');
+    p.innerHTML = a * b;
+
+    resultado.appendChild(p)
+    p.style = 'color: blue'
+}
 
 function resultado (msg) {
     const resultado = document.querySelector('#box2');
@@ -19,7 +37,6 @@ function resultado (msg) {
 
     resultado.appendChild(p)
     p.className = 'teste'
-
 
     const time = document.createElement('p')
 
@@ -31,26 +48,14 @@ function resultado (msg) {
     let dif = Math.floor(result / (1000 * 60 * 60 * 24 * 365.25))
     time.innerHTML = dif //data.getHours();
 
-
     resultado.appendChild(time)
 
     time.style = 'color: blue'
 }
 
-function multiplica(a, b) {
-    const resultado = document.querySelector('#box2');
-    const p = document.createElement('p');
-    p.innerHTML = a * b;
-
-    resultado.appendChild(p)
-    p.style = 'color: blue'
-}
-
 //let atual = new Date();
 
-
 //console.log(atual.toLocaleTimeString() + " " + atual.toDateString())
-
 
 // let numeros = [1, 2, 3, 4, 5, 6]
 
