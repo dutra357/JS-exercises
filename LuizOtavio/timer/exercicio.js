20550b838b1a9272b1f9dff64d91444bcd903702
David Dutra <dadutra@hotmail.com>
2024-05-24 08:17:38 -0300

    Luiz Otavio 66 - Timer exercise concluded

--- LuizOtavio/timer/exercicio.js
+++ LuizOtavio/timer/exercicio.js
@@ -1,13 +1,16 @@
 const contador = document.querySelector('#contador');
+
 const start = document.querySelector('#button1')
 const pause = document.querySelector('#button2')
 const zerar = document.querySelector('#button3')
+
 let segundos = 0;
 let timer;
 
 //No formulário pega o SUBMIT, já para botãoo CLICK
 start.addEventListener('click', function(e){
-    iniciaRelogio()
+    clearInterval(timer);
+    iniciaRelogio();
 })
 
 zerar.addEventListener('click', function(e){
@@ -37,12 +40,14 @@ function iniciaRelogio(){
 
 
 
+
+
 function time(){
     const date = new Date();
-
     contador.innerHTML = date;
 }
 
 function zerarContador(){
+    segundos = 0;
     contador.innerHTML = '00:00:00';
 }
