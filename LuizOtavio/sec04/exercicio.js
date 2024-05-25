edfad40c32accd277c44d33cc2327f26c253742c
David Dutra <dadutra@hotmail.com>
2024-05-25 11:36:39 -0300

    Luiz Otavio lesson 73 - callback functions

--- LuizOtavio/sec04/exercicio.js
+++ LuizOtavio/sec04/exercicio.js
@@ -6,14 +6,40 @@
 
 // func("teste")
 
-let entrada = function entrada(valor1){
-    function interna(valor2){
-        return valor1 + valor2;
-    }
-    return interna;
+// let entrada = function entrada(valor1){
+//     let alfa = 0;
+//     function interna(valor2){
+//         return valor1 * valor2;
+//     }
+//     return interna;
+// }
+
+// const primeira = entrada(5);
+// const segunda = primeira(100);
+
+// console.dir(primeira)
+
+
+
+
+
+function rand(min = 1000, max = 5000) {
+    const num = Math.random() * (max - min) + min;
+    return Math.floor(num);
 }
 
-const primeira = entrada(5);
-const segunda = primeira(2);
+function f1(callback) {
+    setTimeout(function() {
+        console.log('f1');
+        if (callback) callback();
+    }, rand());
+}
 
-console.log(segunda)
+function f2(callback) {
+    setTimeout(function() {
+        console.log('f2');
+        if (callback) callback();
+    }, rand());
+}
+
+f1(f2)
