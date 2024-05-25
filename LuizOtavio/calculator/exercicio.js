6321c3b5ff9e90cc4a50a73f27b0bb6756fbf3c7
David Dutra <dadutra@hotmail.com>
2024-05-25 17:11:56 -0300

    Luiz Otavio lesson 76 - Calculator on JS exercise

---
+++ LuizOtavio/calculator/exercicio.js
@@ -0,0 +1,15 @@
+function startCalc() {
+    //returns a calc object
+    return {
+
+
+        start() {
+            console.log('Olá!')
+        }
+    }
+}
+
+
+
+const calc = startCalc();
+calc.start();
