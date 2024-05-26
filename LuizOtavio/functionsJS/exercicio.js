68393391c6abe6d766b321d020fd3857c854bfbf
David Dutra <dadutra@hotmail.com>
2024-05-26 10:36:07 -0300

    Generator functions - Array slice/splice

--- LuizOtavio/functionsJS/exercicio.js
+++ LuizOtavio/functionsJS/exercicio.js
@@ -10,3 +10,13 @@ console.log(teste.next().value)
 
 let num = 123['toString']()
 console.log(num)
+
+
+
+let nomes = ['david', 'joana', 'maria', 'joana']
+console.log(nomes.slice(0,2))
+
+let nomeCompleto = 'Luiz Otavio Miranda'
+console.log(nomeCompleto.split(' '))
+
+console.log(nomes.join(', '))
