b40111afa577e9ed04d1686811c012f4db4f4045
David Dutra <dadutra@hotmail.com>
2024-05-26 11:39:20 -0300

    Generator functions - Filter() and callbackFunctions

--- LuizOtavio/functionsJS/exercicio.js
+++ LuizOtavio/functionsJS/exercicio.js
@@ -12,18 +12,31 @@ let num = 123['toString']()
 console.log(num)
 
 
-
-let nomes = ['Laura', 'Joana', 'Maria', 'Joana', 'Josoaldo']
+let nomes = ['Laura', 'Joana', 'Maria', 'Fabio', 'Josoaldo']
 console.log(nomes.slice(0,2))
 
-let nomeCompleto = 'Luiz Otavio Miranda'
-console.log(nomeCompleto.split(' '))
 
-console.log(nomes.join(', '))
+let nomeCompleto = 'Luiz Otavio Miranda';
+let split = nomeCompleto.split(' ');
 
 
 //splice - passando o índice de inicio, segundo o n de el a remover
 //splice retorna os removidos
 //Seg arg sendo 0, adiciona no array o 3 argumento (string)
-let nomes2 = nomes.splice(2, 0, 'LUIZ  ')
-console.log(nomes)
+let nomes2 = nomes.splice(0, 0, 'Adalberto')
+
+
+
+//FILTER filtra array, map modifica, e reduce o reduz
+//a função de callback, a ser aplicada, deve retornar true para os valores de interesse
+let filtrar = [5, 50, 80, 1, 2, 3, 4, 5, 8, 7, 11, 15, 22, 17]
+
+function call(valor) {
+    if (valor % 2 == 0) {
+        return true;
+    }else return false;
+}
+
+let filtrados = filtrar.filter(call)
+
+console.log(filtrados)
