1aba5a5ff075300ed683524a7eac079a3170d9f2
David Dutra <dadutra@hotmail.com>
2024-05-26 10:48:13 -0300

    Generator functions - Array splice/slice

--- LuizOtavio/functionsJS/exercicio.js
+++ LuizOtavio/functionsJS/exercicio.js
@@ -13,10 +13,17 @@ console.log(num)
 
 
 
-let nomes = ['david', 'joana', 'maria', 'joana']
+let nomes = ['Laura', 'Joana', 'Maria', 'Joana', 'Josoaldo']
 console.log(nomes.slice(0,2))
 
 let nomeCompleto = 'Luiz Otavio Miranda'
 console.log(nomeCompleto.split(' '))
 
 console.log(nomes.join(', '))
+
+
+//splice - passando o índice de inicio, segundo o n de el a remover
+//splice retorna os removidos
+//Seg arg sendo 0, adiciona no array o 3 argumento (string)
+let nomes2 = nomes.splice(2, 0, 'LUIZ  ')
+console.log(nomes)
