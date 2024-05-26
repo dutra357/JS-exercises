991cd20c4b83d13cab3cd8c5add34a038a6c1338
David Dutra <dadutra@hotmail.com>
2024-05-25 22:12:29 -0300

    Luiz Otavio lesson 76 - Calculator exercise done

--- LuizOtavio/calculator/exercicio.js
+++ LuizOtavio/calculator/exercicio.js
@@ -2,14 +2,11 @@ function startCalc() {
     //returns a calc object
     return {
         display: document.querySelector('.display'),
-
-
-
-
-
+        btnClear: document.querySelector('.btn-clear'),
 
         start() {
             this.clickBtn();
+            this.enterKey();
         },
 
         clickBtn() {
@@ -19,12 +16,58 @@ function startCalc() {
                 if (element.classList.contains('btn-num')) {
                     this.btnStopDisplay(element.innerText);
                 }
+                if (element.classList.contains('btn-clear')) {
+                    this.clearDisplay();
+                }
+
+                if (element.classList.contains('btn-del')) {
+                    this.delOne();
+                }
+
+                if (element.classList.contains('btn-eq')) {
+                    this.makeCalc();
+                }
+                
             }.bind(this));
         },
 
-        btnStopDisplay(valor) {
+        makeCalc() {
+            let problem = this.display.value;
 
-        }
+            try {
+                problem = eval(problem);
+
+                if (!problem) {
+                    alert('Invalid value!')
+                    return;
+                }
+                this.display.value = String(problem);
+
+            } catch (e) {
+                alert('Invalid value!');
+                return;
+            }
+        },
+
+        enterKey() {
+            this.display.addEventListener('keyup', (key) => {
+                if (key.keyCode === 13) {
+                    this.makeCalc();
+                }
+            })
+        },
+
+        btnStopDisplay(value) {
+            this.display.value += value;
+        },
+
+        clearDisplay() {
+            this.display.value = "";
+        },
+
+        delOne() {
+            this.display.value = this.display.value.slice(0, -1);
+        },
     }
 }
 
